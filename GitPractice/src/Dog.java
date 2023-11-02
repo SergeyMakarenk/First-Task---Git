168dfc711f4d3eddb1146892f39290e024642c29
SergeyMakarenk <makaren@tut.by>
2023-11-03 00:31:24 +0300

    feat: add all args constructor in class Dog

--- GitPractice/src/Dog.java
+++ GitPractice/src/Dog.java
@@ -2,4 +2,10 @@ public class Dog {
     private String name;
     private int age;
     private int weight;
+
+    public Dog(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
 }
